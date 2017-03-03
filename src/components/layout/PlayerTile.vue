3d6f639717585af5dd0035ecb285cc12f309017a
Robert Kirsz <robert.kirsz@gmail.com>
2017-03-03 13:59:55 +0100

    Added "PlayerTile" component that will divide screen for each player

---
+++ src/components/layout/PlayerTile.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="player-tile" :style="style">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'player-tile',
+  computed: {
+    numberOfPlayers () {
+      return this.$store.getters.numberOfPlayers
+    },
+    style () {
+      const divider = Math.ceil(this.numberOfPlayers / 2) || 1
+      return { width: `calc(100vw / ${divider})` }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.player-tile {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 50vh;
+}
+
+.player-tile:nth-of-type(even):not(:only-of-type) {
+  transform: rotate(180deg);
+}
+</style>
