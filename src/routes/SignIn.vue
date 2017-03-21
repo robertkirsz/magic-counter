9df29577e978bd8449ec910aa9637abb82d26fd0
Robert Kirsz <robert.kirsz@gmail.com>
2017-03-21 16:49:46 +0100

    Added "SignIn" component

---
+++ src/routes/SignIn.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <form>
+      <input type="text" planceholder="Email" />
+      <input type="text" planceholder="Password" />
+      <button>Sign In</button>
+    </form>
+    <p>Or</p>
+    <div class="social-buttons">
+      <button>Log in with Facebook</button>
+      <button>Log in with Twitter</button>
+      <button>Log in with Google</button>
+      <button>Log in with GitHub</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .social-buttons {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
