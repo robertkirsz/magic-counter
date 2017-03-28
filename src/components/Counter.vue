a11ca2995644e65248e24c7206a0763b5fd8ed44
Robert Kirsz <robert.kirsz@gmail.com>
2017-03-29 01:35:04 +0200

    Fix ID labels position

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -40,7 +40,6 @@
 </template>
 
 <script>
-// TODO: transitions look weird when we go from 10 to 9
 import { mapGetters } from 'vuex'
 import CounterButton from '@/components/CounterButton'
 
@@ -102,98 +101,92 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  // BASE
 
-// BASE
-
-.counter {
-  display: flex;
-  flex-wrap: nowrap;
-  justify-content: space-around;
-  align-items: center;
-  position: relative;
-  font-size: 1em;
-  .label {
-    //
-    position: absolute;
-    font-size: 0.4em;
-    top: -0.9em;
-  }
-  .count {
+  .counter {
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: space-around;
+    align-items: center;
     position: relative;
-    transition: font-size 0.3s;
-    &.moreThan100 {
-      font-size: 6.3em;
+    font-size: 1em;
+    .label {
+      position: absolute;
+      top: -0.9em;
+      left: 50%;
+      font-size: 0.4em;
+      transform: translateX(-50%);
     }
-    .placeholder {
-      opacity: 0;
+    .count {
+      position: relative;
+      transition: font-size 0.3s;
+      &.moreThan100 {
+        font-size: 6.3em;
+      }
+      .placeholder {
+        opacity: 0;
+      }
+      .value {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        text-align: center;
+      }
     }
-    .value {
+  }
+
+  // MODIFIERS
+
+  .counter--life {
+    max-height: 50vh;
+    font-size: 7em;
+    @media (max-width: 420px) {
+      font-size: 9em;
+    }
+  }
+
+  .counter--poison,
+  .counter--commander {
+    padding: 0.1em;
+    font-weight: 800;
+    z-index: 1;
+    .count {
+      font-size: 0.6em;
+    }
+    .counter-buttons {
+      padding: 0.3em 0.5em;
+    }
+    &::after {
+      content: "";
       position: absolute;
       top: 0;
       right: 0;
       bottom: 0;
       left: 0;
-      text-align: center;
+      background-repeat: no-repeat;
+      background-size: contain;
+      opacity: 0.25;
+      z-index: -1;
     }
   }
-}
 
-// MODIFIERS
+  .counter--poison {
+  	&::after {
+      top: -10px;
+      right: -10px;
+      bottom: -10px;
+      left: -10px;
+  		background-image: url("../assets/poison.svg");
+  		background-position: 50% 50%;
+  	}
+  }
 
-.counter--life {
-  max-height: 50vh;
-  font-size: 7em;
-  // background-color: rgba(0, 255, 255, 0.2);
-  // border: 2px solid rgb(0, 255, 255);
-  @media (max-width: 420px) {
-    font-size: 9em;
+  .counter--commander {
+  	&::after {
+  		background-image: url("../assets/commander.svg");
+  		background-position: 50% 35%;
+  	}
   }
-  .count {
-    // background-color: rgba(255, 255, 0, 0.2);
-    // border: 2px solid rgb(255, 255, 0);
-  }
-}
-
-.counter--poison,
-.counter--commander {
-  padding: 0.1em;
-  font-weight: 800;
-  z-index: 1;
-  .count {
-    font-size: 0.6em;
-  }
-  .counter-buttons {
-    padding: 0.3em 0.5em;
-  }
-  &::after {
-    content: "";
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    background-repeat: no-repeat;
-    background-size: contain;
-    opacity: 0.25;
-    z-index: -1;
-  }
-}
-
-.counter--poison {
-	&::after {
-    top: -10px;
-    right: -10px;
-    bottom: -10px;
-    left: -10px;
-		background-image: url("../assets/poison.svg");
-		background-position: 50% 50%;
-	}
-}
-
-.counter--commander {
-	&::after {
-		background-image: url("../assets/commander.svg");
-		background-position: 50% 35%;
-	}
-}
 </style>
